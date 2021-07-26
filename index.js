77f103049e734a0d6cec786b3f5738fdead0c49b
Semyonova Alexandra <semeenowa.alexandra@yandex.ru>
2021-07-26 20:21:40 +0300

    fullscreen

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+let burgerbutton = document.querySelector("#burgerbutton")
+let fullscreenmenu = document.querySelector("#fullscreenmenu")
+let link = document.querySelectorAll(".fullscreen-menu__link")
+let close = document.querySelector("#closebutton");
+
+burgerbutton.addEventListener("click", function(e) {
+    e.preventDefault()
+});
+for (var i = 0; i < link.length; i++)
+    link[i].addEventListener("click", function(e) {
+        fullscreenmenu.style.display = "none"
+    });
+    burgerbutton.addEventListener("click", function() {
+        fullscreenmenu.style.display = "block"
+}),
+close.addEventListener("click", function() {
+    fullscreenmenu.style.display = "none"
+});
+
+
+
 const left = document.querySelector("#left")
 const right = document.querySelector("#right")
 const innerProducts = document.querySelector("#innerProducts");
