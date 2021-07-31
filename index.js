b4c7862c0b7ac70e691a639274ba3087a06f7714
Semyonova Alexandra <semeenowa.alexandra@yandex.ru>
2021-07-31 11:26:56 +0300

    fourth week

--- index.js
+++ index.js
@@ -39,36 +39,9 @@ $(".paginator__link").click((e) => {
 });
 
 
-const openItem = item => {
-  const container = item.closest(".team__item");
-  const contentBlock = container.find(".team__content");
-  const textBlock = contentBlock.find(".team__content-block");
-  const reqHeight = textBlock.height();
 
-  container.addClass("active-item");
-  contentBlock.height(reqHeight);
-}
-
-const closeEveryItem = container => {
-  const items = container.find('.team__content');
-  const itemContainer = container.find(".team__item");
-
-  itemContainer.removeClass("active")
-  items.height(0);
-}
-
-
-$('.team__name').click(e => {
-  const $this = $(e.currentTarget);
-  const container = $this.closest('.team');
-  const elemContainer = $this.closest(".team__item");
-
-  if (elemContainer.hasClass("active-item")) {
-    closeEveryItem(container);
-  } else {
-    closeEveryItem(container);
-    openItem($this);
-  }
+$(".team__item").on("click", function (event) {
+  $(this).toggleClass("is_open").siblings().removeClass('is_open');
 });
 
 const slider = $('.products__slider').bxSlider({
@@ -84,81 +57,133 @@ $('.left-scroll-button').click(e => {
 $('.right-scroll-button').click(e => {
   e.preventDefault();
   slider.goToNextSlide();
-});
-
-
-const validateFields = (form, fieledsArray) => {
-
-  fieledsArray.forEach((filed) => {
-    filed.removeClass("input-error");
-    if (field.val().trim() == "") {
-      field.addClass("input-error");
-    }
   });
 
-  const errorFields = form.find(".input-error");
+  const validateFields = (form, fieledsArray) => {
 
-  return errorFields.length == 0;
-}
-
-$(".form__content").submit((e) => {
-  e.preventDefault();
-
-  const form = $(e.currentTarget);
-  const name = form.find("[name='name']");
-  const phone = form.find("[name='phone']");
-  const comment = form.find("[name='comment']");
-  const to = form.find("[name='to']");
-
-  const modal = $("#modal");
-  const content = modal.find(".modal__content");
-
-  modal.removeClass("error-modal");
-
-  const isValid = validateFields(form, [name, phone, comment.to]);
-
-
-  if (isValid) {
-    const request = $.ajax({
-      url: "https://webdev-api.loftschool.com/sendmail",
-      method: "post",
-      data: {
-        name: name.val(),
-        phone: phone.val(),
-        comment: comment.val(),
-        to: to.val(),
-      },
-
-
-      error: data => { }
-    });
-
-    request.done((data) => {
-      content.text(data.message);
-
-    });
-
-    request.fail((data) => {
-      const message = data.responseJSON.message;
-      content.text(message);
-      modal.addClass("error-modal");
-
-
-
-    });
-
-    request.always(() => {
-      $.fancybox.show({
-        src: "#modal",
-        type: "inline"
+      fieledsArray.forEach((filed) => {
+        filed.removeClass("input-error");
+        if (field.val().trim() == "") {
+          field.addClass("input-error");
+        }
       });
-    });
-  }
+    
+      const errorFields = form.find(".input-error");
+    
+      return errorFields.length == 0;
+    }
+  
+    $(".form__content").submit(e => {
+    e.preventDefault();
+    const form = $(e.currentTarget);
+    const name = form.find("[name='name']");
+    const phone = form.find("[name='phone']");
+    const comment = form.find("[name='comment']");
+    const to = form.find("[name='to']");
+    // const modal = $("#modal");
+    // const content = modal.find(".modal-content");
+    // modal.removeClass("error-modal");
+    const isValid = validateFields(form, [name, phone, comment, to]);
+    if (isValid) {
+        const request = $.ajax({
+            url: "https://webdev-api.loftschool.com/sendmail",
+            method: "post",
+            data: {
+                name: name.val(),
+                phone: phone.val(),
+                comment: comment.val(),
+                to: to.val()
+            }
+        });
+        request.done((data) => {
+            content.text(data.message);
+            e.target.reset();
+        });
+        request.fail((data) => {
+            const message = data.responseJSON.message;
+            content.text(message);
+            // modal.addClass("error-modal");
+        });
+        request.always(() => {
+            $.fancybox.show({
+                src: "#modal",
+                type: "inline"
+            });
+        })
+    }
 });
+// const validateFields = (form, fieledsArray) => {
 
-$(".app-submit-button").click(e => {
-  e.preventDefault();
+//   fieledsArray.forEach((filed) => {
+//     filed.removeClass("input-error");
+//     if (field.val().trim() == "") {
+//       field.addClass("input-error");
+//     }
+//   });
 
-  $.fancybox.close();
+//   const errorFields = form.find(".input-error");
 
-})
+//   return errorFields.length == 0;
+// }
+
+// $(".form__content").submit((e) => {
+//   e.preventDefault();
+
+//   const form = $(e.currentTarget);
+//   const name = form.find("[name='name']");
+//   const phone = form.find("[name='phone']");
+//   const comment = form.find("[name='comment']");
+//   const to = form.find("[name='to']");
+
+//   const modal = $("#modal");
+//   const content = modal.find(".modal__content");
+
+//   modal.removeClass("error-modal");
+
+//   const isValid = validateFields(form, [name, phone, comment.to]);
+
+
+//   if (isValid) {
+//     const request = $.ajax({
+//       url: "https://webdev-api.loftschool.com/sendmail",
+//       method: "post",
+//       data: {
+//         name: name.val(),
+//         phone: phone.val(),
+//         comment: comment.val(),
+//         to: to.val(),
+//       },
+
+
+//       error: data => { }
+//     });
+
+//     request.done((data) => {
+//       content.text(data.message);
+
+//     });
+
+//     request.fail((data) => {
+//       const message = data.responseJSON.message;
+//       content.text(message);
+//       modal.addClass("error-modal");
+
+
+
+//     });
+
+//     request.always(() => {
+//       $.fancybox.show({
+//         src: "#modal",
+//         type: "inline"
+//       });
+//     });
+//   }
+// });
+
+// $(".app-submit-button").click(e => {
+//   e.preventDefault();
+
+//   $.fancybox.close();
+
+// })
