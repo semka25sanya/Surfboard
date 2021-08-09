eb1de9091678f936e2825d3d8bebcc2e37dd35c1
Semyonova Alexandra <semeenowa.alexandra@yandex.ru>
2021-08-09 21:51:16 +0300

    5 week

--- index.js
+++ index.js
@@ -109,54 +109,137 @@ $(".form__content").submit(e => {
 $(".app-submit-button").on("click", function (event) {
     $('.modal').removeClass("open");
 });
-//   const validateFields = (form, fieldsArray) => {
-//     fieldsArray.forEach((field) => {
-//         if (field.val().trim() == "") {
-//             field.addClass("input-error");
-//         } else {
-//             field.removeClass("input-error");
-//         }
-//     });
-//     const errorFields = form.find(".input-error");
-//     return errorFields.length == 0;
-// }
-//     $(".form__content").submit(e => {
-//     e.preventDefault();
 
-//     const form = $(e.currentTarget);
-//     const name = form.find("[name='name']");
-//     const phone = form.find("[name='phone']");
-//     const comment = form.find("[name='comment']");
-//     const to = form.find("[name='to']");
-//     // const modal = $("#modal");
-//     // const content = modal.find(".modal-content");
-//     // modal.removeClass("error-modal");
-//     const isValid = validateFields(form, [name, phone, comment, to]);
-//     if (isValid) {
-//         const request = $.ajax({
-//             url: "https://webdev-api.loftschool.com/sendmail",
-//             method: "post",
-//             data: {
-//                 name: name.val(),
-//                 phone: phone.val(),
-//                 comment: comment.val(),
-//                 to: to.val()
-//             }
-//         });
-//         request.done((data) => {
-//             content.text(data.message);
-//             e.target.reset();
-//         });
-//         request.fail((data) => {
-//             const message = data.responseJSON.message;
-//             content.text(message);
-//             // modal.addClass("error-modal");
-//         });
-//         request.always(() => {
-//             $.fancybox.open({
-//                 src: "#modal",
-//                 type: "inline"
-//             });
-//         })
-//     }
-// });
+let player;
+const playerContainer = $(".video-player");
+
+let eventsInit = () => {
+  $(".video-player__icon--start").click(e =>{
+    e.preventDefault();
+
+    if(playerContainer.hasClass("paused")) {
+      playerContainer.removeClass("paused");
+      player.pauseVideo();
+    } else {
+      playerContainer.addClass("paused");
+        player.playVideo();
+
+    }
+  });
+
+
+$(".video-player__lenght-scale").click(e => {
+  const bar = $(e.currentTarget);
+  const clickedPosition = e.originalEvent.layerX;
+  const newButtonPositionPercent = (clickedPosition / bar.width()) * 100 ;
+ const newPlayPositionSec = (player.getDuration()/100) * newButtonPositionPercent;
+
+ $(".video-player__circle").css({
+   left: `${newButtonPositionPercent}%`
+ });
+ player.seekTo(newPlayPositionSec);
+});
+};
+
+
+ $(".video-player__splash").click(e => {
+   player.playVideo();
+ })
+
+
+const onPlayerStateChange = event => {
+  switch (event.data) {
+    case 1:
+      playerContainer.addClass("video-player--active");
+      playerContainer.addClass("video-player--paused")
+      break;
+
+      case 2:
+        playerContainer.removeClass("video-player--active");
+        playerContainer.removeClass("video-player--paused")
+        break;
+
+  }
+};
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('yt-video-player', {
+          height: '405',
+          width: '660',
+          videoId: 'LXb3EKWsInQ',
+          events: {
+            // 'onReady': onPlayerReady,
+            // 'onStateChange': onPlayerStateChange
+          },
+          playerVars: {
+            controls:0,
+            disablekb:0,
+            showinfo:0,
+            rel:0,
+            autoplay:0,
+            modestbranding:0
+          }
+        });
+      }
+
+      eventsInit();
+    
+    let myMap;
+
+    const init = () => {
+      myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 7
+      });
+    }
+
+    ymaps.ready(init);
+
+    const verticalAcc = () => {
+      const links = document.querySelectorAll(".menu-section__link");
+      const body = document.querySelector('body');
+
+      const calculateWidth = () => {
+        const windowWidth = window.innerWidth;
+
+        const MAX_WIDTH = 550;
+
+        const linksWidth = links[0].offsetWidth;
+
+        const reqWidth = windowWidth - (linksWidth * links.length);
+
+        return reqWidth > MAX_WIDTH ? MAX_WIDTH : reqWidth;
+    
+      };
+
+      function closeItem(activeElement) {
+        const activeText = activeElement.querySelector(".menu-section__content");
+        activeText.style.width = "0px";
+        activeElement.classList.remove("active--item");
+      }
+      links.forEach(function(elem){
+        elem.addEventListener("click", function(e){
+          e.preventDefault();
+          const link = e.target.closest(".menu-section__link");
+
+          const active = document.querySelector("active--item");
+          console.log(active)
+
+          if(active) {
+            closeItem(active);
+          }
+
+          if (!active || active.querySelector(".menu-section__link") !== link) {
+            const current = link.closest(".menu-section__item");
+          current.classList.add("active--item");
+          const currentText = current.querySelector(".menu-section__content");
+          if (body.offsetWidth> 480) {
+            currentText.style.width = calculateWidth() + 'px';
+
+          } else {
+            currentText.style.width = '100%';
+          }
+          }
+        });
+
+      });
+    }
