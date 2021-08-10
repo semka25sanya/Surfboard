0bf2715d66df4becbc90e24fdc17ff09358973d5
Semyonova Alexandra <semeenowa.alexandra@yandex.ru>
2021-08-10 19:23:46 +0300

    mobile ops

--- index.js
+++ index.js
@@ -268,85 +268,127 @@ ymaps.ready(init);
 
   const sections = $("section");
   const display = $(".wrapper__content");
+  const sideMenu = $(".fixed-menu");
+  const menuItems = sideMenu.find(".fixed-menu__item");
+
+  const mobileDetect = new MobileDetect(window.navigator.userAgent);
+  const isMobile = mobileDetect.mobile();
 
 
 let inScroll = false;
 
 sections.first().addClass("active-section");
 
-  const performTransition = (sectionEq) => {
-    if (inScroll === false) {
-inScroll = true;
-    
+  const countSectionPosition = (sectionEq) => {
     const position = sectionEq * -100;
+    
+    if (isNaN(position)) {
+      return 0;
+    }
 
+    return position;
+  };
+    
+    const changeMenuThemeForSection = (sectionEq) => {
     const currentSection = sections.eq(sectionEq);
     const menuTheme = currentSection.attr("data-sidemenu-theme");
-    const sideMenu = $(".fixed-menu");
+    const activeClass = "fixed-menu--shadowed";
+    
 
     if (menuTheme === "black") {
-      sideMenu.addClass("fixed-menu--shadowed");
+      sideMenu.addClass(activeClass);
     } else {
-      sideMenu.removeClass("fixed-menu--shadowed");
+      sideMenu.removeClass(activeClass);
     }
+  };
+
+  const resetActiveClassForItem = (items, itemEq, activeClass)  => {
+    items.eq(itemEq).addClass(activeClass).siblings().removeClass(activeClass);
+  }
+
+  const performTransition = (sectionEq) => {
+    if (inScroll) return;
+
+    const transitionOver = 1000;
+    const mouseInertiaOver = 300;
+
+    inScroll = true;
+
+      const position = countSectionPosition(sectionEq);
+   
+      changeMenuThemeForSection(sectionEq);
 
     display.css({
       transform: `translateY(${position}%)`
     });
 
-    sections.eq(sectionEq).addClass("active-section").siblings().removeClass("active-section");
+    resetActiveClassForItem(sections, sectionEq, "active-section");
     
     
     
     setTimeout(() => {
       inScroll = false;
- sideMenu.find(".fixed-menu__item").eq(sectionEq).addClass("fixed-menu__item--active").siblings().removeClass("fixed-menu__item--active");
-    }, 1300);
+
+      resetActiveClassForItem(menuItems, sectionEq, "active-section");
+    }, transitionOver + mouseInertiaOver);
   
-  }
+  
   };
 
 
-  const scrollViewport = direction => {
+  const viewportScroller = () => {
     const activeSection = sections.filter(".active-section");
     const nextSection = activeSection.next();
     const prevSection = activeSection.prev();
 
-    if (direction === "next" && nextSection.length) {
-performTransition(nextSection.index());
-    }
-    if (direction === "prev" && prevSection.length) {
-performTransition(prevSection.index());
-    }
+    return {
+      next() {
+        if (nextSection.length) {
+        performTransition(nextSection.index());
+            }
+
+      },
+    prev() {
+      if (prevSection.length) {
+        performTransition(prevSection.index());
+            }
+    },
+  };
   };
 
   $(window).on("wheel", (e) => {
     const deltaY = e.originalEvent.deltaY;
+    const scroller = viewportScroller();
 
     if (deltaY > 0) {
-      scrollViewport("next");
+     scroller.next();
     }
     if (deltaY < 0) {
-      scrollViewport("prev");
+      scroller.prev();
     }
 
   });
 
   $(window).on("keydown", (e) => {
     const tagName = e.target.tagName.toLowerCase();
+    const userTypingInInputs = tagName === "input" || tagName === "textarea";
+    const scroller = viewportScroller();
 
-    if (tagName !== "input" && tagName !== "textarea") {
+    if (userTypingInInputs) return;
+    
      switch (e.keyCode) {
        case 38:
-         scrollViewport ("prev");
+         scroller.prev();
          break;
 
          case 40:
-           scrollViewport("next");
+          scroller.next();
            break;
      }
-     }
+     
   });
+
+  $(".wrapper").on("touchmove", e => e.preventDefault());
 
   $("[data-scroll-to]").click(e => {
     e.preventDefault();
@@ -357,4 +399,19 @@ performTransition(prevSection.index());
 
     performTransition(reqSection.index());
   });
+if (isMobile) {
+    $("body").swipe( {
+      
+      swipe:function(event, direction) {
+       const scroller = viewportScroller();
+       let scrollDirection = "";
 
+       if (direction === "up" ) scrollDirection = "next";
+       if (direction === "down" ) scrollDirection = "prev";
+      
+       scroller[scrollDirection]();
+      },
+    });
+  }
+  
+    
